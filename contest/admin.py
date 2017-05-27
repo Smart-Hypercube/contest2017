ec0cc9a82a9a3cb429a2f74054a5ce18c6c55dba
Hypercube <hypercube@0x01.me>
2017-05-27 22:53:27 +0800

    'update'

--- contest/admin.py
+++ contest/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Group, Ticket, User, Contest, School, Contestant, Participate, Vote
+from .models import Group, Ticket, User, Contest, Current, School, Contestant, Participate, Vote
 
 
-admin.site.register((Group, Ticket, User, Contest, School, Contestant, Participate, Vote))
+admin.site.register((Group, Ticket, User, Contest, Current, School, Contestant, Participate, Vote))
--- contest/views.py
+++ contest/views.py
@@ -1,26 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_http_methods
+from django.views.decorators.http import require_http_methods, require_safe
 
 from django.core.exceptions import ObjectDoesNotExist, ValidationError
 
-from .models import User, Ticket, Group, Current, Contest
+from .models import *
 
 import bs4
 
 
+@require_safe
 def query(request):
     context = {'paid': request.GET.get('paid', False),
                'contests': sorted(list(Contest.objects.all()), key=lambda c: c.order)}
     return render(request, 'contest/query.html', context)
 
 
+@require_safe
 def checklist(request):
     context = {'contests': sorted(list(Contest.objects.all()), key=lambda c: c.order)}
     return render(request, 'contest/checklist.html', context)
 
 
+@require_safe
 def scoretable(request):
     context = {'contests': sorted(list(Contest.objects.all()), key=lambda c: c.order)}
     return render(request, 'contest/scoretable.html', context)
@@ -30,8 +33,12 @@ def get_number(s):
     return ''.join(filter(str.isdigit, s))
 
 
-def subscribe(request, data):
-    return render(request, 'contest/reply.xml', {'from': 'from', 'to': 'to', 'content': 'content'})
+def subscribe(request, msg):
+    context = {'msg': msg,
+               'current': Current.objects.get().contest,
+               'result': False,
+               'reason': 0}
+    return render(request, 'contest/login.xml', context)
 
 
 def login(request, msg):
@@ -47,7 +54,7 @@ def login(request, msg):
                    'reason': 0}
         return render(request, 'contest/login.xml', context)
     try:
-        user = User(group=Group.objects.get(name='audience'),
+        user = User(group=Group.objects.get(id=2),
                     ticket=Ticket.objects.get(number=ticket),
                     wechat=msg['from'])
         user.save()
@@ -70,26 +77,80 @@ def login(request, msg):
 
 
 def text(request, msg, user):
-    input = get_number(msg['content'])
-    if input == 0:
+    content = get_number(msg['content'])
+    if content == 0:
         context = {'msg': msg,
                    'current': Current.objects.get().contest}
         return render(request, 'contest/base.xml', context)
     current = Current.objects.get().contest
-    if len(ticket) != 4 or len(ticket) + 10 < len(msg['content']):
+    if len(content) != 4 or len(content) + 10 < len(msg['content']):
         context = {'msg': msg,
                    'current': Current.objects.get().contest,
                    'result': False,
                    'reason': 0}
         return render(request, 'contest/vote.xml', context)
+    participate = None
     try:
-        current.participate_set.get(contestant1__number=input)
+        participate = current.participate_set.get(contestant1__number=content)
     except ObjectDoesNotExist:
         pass
+    try:
+        participate = current.participate_set.get(contestant2__number=content)
+    except ObjectDoesNotExist:
+        pass
+    if participate:
+        vote = None
+        try:
+            vote = Vote.objects.get(participate__contest=participate.contest)
+        except ObjectDoesNotExist:
+            pass
+        if vote:
+            context = {'msg': msg,
+                       'current': Current.objects.get().contest,
+                       'result': False,
+                       'reason': 1}
+            return render(request, 'contest/vote.xml', context)
+        Vote(user=user, participate=participate).save()
+        context = {'msg': msg,
+                   'current': Current.objects.get().contest,
+                   'result': True}
+        return render(request, 'contest/vote.xml', context)
+    try:
+        participate = current.prev().participate_set.get(contestant1__number=content)
+    except ObjectDoesNotExist:
+        pass
+    try:
+        participate = current.prev().participate_set.get(contestant2__number=content)
+    except ObjectDoesNotExist:
+        pass
+    if participate:
+        vote = None
+        try:
+            vote = Vote.objects.get(participate__contest=participate.contest)
+        except ObjectDoesNotExist:
+            pass
+        if vote:
+            context = {'msg': msg,
+                       'current': Current.objects.get().contest,
+                       'result': False,
+                       'reason': 1}
+            return render(request, 'contest/vote.xml', context)
+        Vote(user=user, participate=participate).save()
+        context = {'msg': msg,
+                   'current': Current.objects.get().contest,
+                   'result': True}
+        return render(request, 'contest/vote.xml', context)
+    context = {'msg': msg,
+               'current': Current.objects.get().contest,
+               'result': False,
+               'reason': 2}
+    return render(request, 'contest/vote.xml', context)
 
 
-def admin(request, msg, user):
-    pass
+def admin(request, msg, _):
+    context = {'msg': msg,
+               'current': Current.objects.get().contest}
+    return render(request, 'contest/base.xml', context)
 
 
 @csrf_exempt
@@ -99,14 +160,16 @@ def webhook(request):
         data = bs4.BeautifulSoup(request.body, 'html.parser').xml
         if data.msgtype.text == 'event':
             if data.event.text == 'subscribe':
-                return subscribe(request, data)
+                msg = {'from': data.fromusername.text,
+                       'to': data.tousername.text}
+                return subscribe(request, msg)
         elif data.msgtype.text == 'text':
             msg = {'from': data.fromusername.text,
                    'to': data.tousername.text,
                    'time': data.createtime.text,
                    'content': data.content.text}
             try:
-                return admin(request, msg, User.objects.get(wechat=msg['from'], group__name='admin'))
+                return admin(request, msg, User.objects.get(wechat=msg['from'], group_id=4))
             except ObjectDoesNotExist:
                 try:
                     return text(request, msg, User.objects.get(wechat=msg['from']))
